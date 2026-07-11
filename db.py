6ad98b8dc235d7a78601de94d77afe811e46452a
seyed.homay@gmail.com <alihomayouni96@gmail.com>
2026-07-11 09:51:42 +0330

    start sqlalchemy in test mode not in git

--- db.py
+++ db.py
@@ -17,7 +17,7 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
 
     conn.execute("PRAGMA foreign_keys = ON;")
-
+    
     # print("Connected to Database Succesfully")
     logging.info("Connected to Database Succesfully")
 
